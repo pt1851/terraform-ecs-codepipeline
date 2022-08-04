c5776e91cda599316973bcabb8991bdf64779961
AkashSivakumar-Dev <82136973+AkashSivakumar-Dev@users.noreply.github.com>
2022-08-05 02:50:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ POLICY
 resource "aws_codebuild_project" "codebuild" {
   name          = "${var.common_name}-${var.image_name}"
   description   = "${var.common_name}-${var.image_name}"
-  build_timeout = "5"
+  build_timeout = var.codebuild_build_timeout
   service_role  = aws_iam_role.codebuild.arn
 
   artifacts {
